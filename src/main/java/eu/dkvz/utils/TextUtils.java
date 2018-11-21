5c0b05681d31ba3596a612a87c21334bc5da41de
DkVZ <bouc@clanz.be>
2018-11-22 00:31:54 +0100

    Got a working regex to detect relative urls (uh... Have to test it on absolute though)

---
+++ src/main/java/eu/dkvz/utils/TextUtils.java
@@ -0,0 +1,20 @@
+package eu.dkvz.utils;
+
+import java.util.regex.*;
+
+public class TextUtils {
+	
+	public static String processRelativeUrls(String content, String baseUrl) {
+		// Looking for text that matches both src="" and href="".
+		// We could look for single quotes too but I won't.
+		// TODO Detect possible whitespaces before and after the "=".
+		Pattern urls = Pattern.compile("(src=\"|href=\")(?!https?://)(.*?)\"", Pattern.MULTILINE);
+		Matcher matcher = urls.matcher(content);
+		if (matcher.find()) {
+			return matcher.group(2);
+		} else {
+			return "No matches";
+		}
+	}
+	
+}
